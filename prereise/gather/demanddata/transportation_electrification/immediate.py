11b4be2854c396a09f9d81dee9edf403a5c29561
Daniel Olsen <daniel@breakthroughenergy.org>
2022-02-23 20:50:03 -0800

    refactor: rewrite immediate_charging to reduce looping

--- prereise/gather/demanddata/transportation_electrification/immediate.py
+++ prereise/gather/demanddata/transportation_electrification/immediate.py
@@ -1,8 +1,103 @@
 import numpy as np
 
 from prereise.gather.demanddata.transportation_electrification import const
-from prereise.gather.demanddata.transportation_electrification import charging
 from prereise.gather.demanddata.transportation_electrification import data_helper
+
+
+allowed_locations_by_strategy = {
+    1: {1},  # home only
+    2: set(range(1, 13)),  # home and go to work related (1-12, inclusive)
+    # We don't need to set the strategy for 3 (everywhere), it's handled elsewhere
+    4: {1, 21},  # home and school only
+    5: {1, 11, 12, 21},  # home and work and school
+}
+
+
+def calculate_charging(trips, charging_power, battery_capacity, kwhmi):
+    """Parse travel patterns to estimate charging and state-of-charge after each trip.
+
+    :param pandas.DataFrame trips: trip data.
+    :param int/float charging_power: charging power (kW).
+    :param int/float battery_capacity: battery capacity (kWh).
+    :param int/float kwhmi: vehicle electricity consumption (kWh/ mile).
+    """
+    # Add trip number entries
+    trips["trip number"] = trips.groupby("sample vehicle number").cumcount() + 1
+
+    grouped_trips = trips.groupby("trip number")
+    for trip_num, group in grouped_trips:
+        if trip_num == 1:
+            # For the first trip, we assume that the vehicle starts with a full battery
+            trips.loc[group.index, "trip start battery charge"] = battery_capacity
+        else:
+            # For subsequent trips, the starting SoC depends on the previous trip
+            relevant_vehicles = group["sample vehicle number"]
+            previous_group = grouped_trips.get_group(trip_num - 1)
+            start_soc = (
+                previous_group["trip end battery charge"]
+                + previous_group["charging consumption"]
+            )
+            trips.loc[group.index, "trip start battery charge"] = start_soc.values[
+                previous_group["sample vehicle number"].isin(relevant_vehicles)
+            ]
+        # For all trips, update ending state of charge
+        trips.loc[group.index, "trip end battery charge"] = (
+            trips.loc[group.index, "trip start battery charge"]
+            - group["Miles traveled"] * kwhmi * const.ER
+        )
+        # Calculate charging duration/energy for the trips that can charge
+        trips.loc[group.index, "full_charge_time"] = (
+            battery_capacity - trips["trip end battery charge"]
+        ) / (charging_power * const.charging_efficiency)
+        trips.loc[group.index, "charging time"] = trips["charging_allowed"] * (
+            trips[["full_charge_time", "Dwell time (hour decimal)"]].apply(min, axis=1)
+        )
+        trips.loc[group.index, "charging consumption"] = (
+            trips["charging time"] * charging_power * const.charging_efficiency
+        )
+
+
+def resample_daily_charging(trips, charging_power):
+    """Translate start and end times and power to a 48-hour output array.
+
+    :param pandas.DataFrame trips: trip data with trip-end and charge-time columns.
+    :param int/float charging_power: charging power (kW).
+    :return: (*numpy.array*) -- hourly total charging power for the 48-hour span.
+    """
+    fine_resolution = 4800
+    coarse_resolution = 48
+    ratio = int(fine_resolution / coarse_resolution)
+    # determine timing of charging
+    augmented_trips = trips.assign(
+        start_point=(ratio * trips["End time (hour decimal)"]).map(round),
+        elapsed=(ratio * trips["charging time"]).map(round),
+        end_point=lambda x: x["start_point"] + x["elapsed"],
+    )
+    # Translate times to fine-resolution arrays
+    indices = np.arange(fine_resolution)
+    indiv_charging_profiles = np.zeros((len(trips), fine_resolution), dtype=bool)
+    for i, (trip_id, trip) in enumerate(augmented_trips.iterrows()):
+        indiv_charging_profiles[i, trip["start_point"] : trip["end_point"]] = True
+    # Sum fine-resolution arrays for each trip into one aggregate array
+    total_profile = indiv_charging_profiles.sum(axis=0) * charging_power
+
+    # Resample fine-resolution arrays into a coarse-resolution array
+    output_array = np.zeros(coarse_resolution)
+    for k in range(coarse_resolution):
+        if k == 0:
+            # First hour, normal sum
+            output_array[k] = sum(total_profile[:ratio]) / ratio
+        elif k == coarse_resolution - 1:
+            # Last hour, normal sum
+            output_array[k] = sum(total_profile[(-1 * ratio) :]) / ratio
+        else:
+            # Every other hour: sum from the half hour before to the half hour after
+            output_array[k] = (
+                sum(total_profile[int((k - 0.5) * ratio) : int((k + 0.5) * ratio)])
+                / 100
+            )
+
+    return output_array
 
 
 def immediate_charging(
@@ -21,264 +116,81 @@ def immediate_charging(
     :param str veh_type: determine which category (LDV or LDT) to produce charging profiles for
     :return: (*numpy.ndarray*) -- charging profiles.
     """
+    # Constants
+    trip_strategy = 1
+    battery_capacity = kwhmi * veh_range
+    input_day = data_helper.get_input_day(data_helper.get_model_year_dti(model_year))
 
     # load NHTS data from function
     if veh_type.lower() == "ldv":
-        newdata = mileage.remove_ldt(mileage.load_data(census_region))
+        trips = data_helper.remove_ldt(data_helper.load_data(census_region))
 
     elif veh_type.lower() == "ldt":
-        newdata = mileage.remove_ldv(mileage.load_data(census_region))
+        trips = data_helper.remove_ldv(data_helper.load_data(census_region))
 
     # updates the weekend and weekday values in the nhts data
-    newdata = mileage.update_if_weekend(newdata)
+    trips = data_helper.update_if_weekend(trips)
 
     # add new columns to newdata to store data that is not in NHTS data
-    newdata = newdata.reindex(
-        list(newdata.columns)
-        + [
-            "trip start battery charge",
-            "trip end battery charge",
-            "charging power",
-            "charging time",
-            "charging consumption",
-            "BEV could be used",
-            "trip number",
-        ],
-        axis="columns",
-        fill_value=0,
+    new_columns = [
+        "trip start battery charge",
+        "trip end battery charge",
+        "charging time",
+        "charging consumption",
+        "BEV could be used",
+        "trip number",
+    ]
+    trips = trips.reindex(list(trips.columns) + new_columns, axis=1, fill_value=0)
+    # Add flag for whether the total mileage is within the vehicle's range
+    trips["BEV could be used"] = (
+        trips["total vehicle miles traveled"] < veh_range * const.ER
     )
+    # Add booleans for whether the location allows charging
+    if location_strategy == 3:
+        trips["location_allowed"] = True
+    else:
+        allowed = allowed_locations_by_strategy["location_strategy"]
+        trips["location_allowed"] = trips["why to"].map(lambda x: x in allowed)
+    # Add booleans for whether the trip number (compared to total trips) allows charging
+    if trip_strategy == 1:
+        trips["trip_allowed"] = True
+    elif trip_strategy == 2:
+        trips["trip_allowed"] = trips["trip number"] == trips["total vehicle trips"]
+    # Add booleans for whether the dell time is long enough to allow charging
+    trips["dwell_allowed"] = trips["Dwell time (hour decimal)"] > 0.2
+    # Add boolean for whether this trip allows charging
+    allowed_cols = ["location_allowed", "trip_allowed", "dwell_allowed"]
+    trips["charging_allowed"] = trips[allowed_cols].apply(all, axis=1)
 
-    input_day = mileage.get_input_day(mileage.get_model_year_dti(model_year))
+    # Evaluate weekend vs. weekday for each trip
+    data_day = data_helper.get_data_day(trips)
+    weekday_trips = trips.loc[data_day == 2].copy()
+    weekend_trips = trips.loc[data_day == 1].copy()
 
-    TRANS_charge = np.zeros(24 * len(input_day))
-    data_day = mileage.get_data_day(newdata)
+    # Calculate the charge times and SOC for each trip, then resample resolution
+    calculate_charging(weekday_trips, power, battery_capacity, kwhmi)
+    calculate_charging(weekend_trips, power, battery_capacity, kwhmi)
+    daily_resampled_profiles = {
+        "weekday": resample_daily_charging(weekday_trips, power),
+        "weekend": resample_daily_charging(weekend_trips, power),
+    }
 
-    trip_strategy = 1
-    kwh = kwhmi * veh_range
-    weekday_electricload, weekend_electricload = np.zeros(4800), np.zeros(4800)
-
-    l = len(newdata)
-
-    # weekday first
-    trip_num = 1
-    start_point = 0
-    end_point = 0
-
-    for i in range(l):
-        if data_day[i] == 2:  # 2 is weekday
-            if (
-                i > 0
-                and newdata.iloc[i, newdata.columns.get_loc("sample vehicle number")]
-                == newdata.iloc[i - 1, newdata.columns.get_loc("sample vehicle number")]
-            ):
-                newdata.iloc[
-                    i, newdata.columns.get_loc("trip start battery charge")
-                ] = (
-                    newdata.iloc[
-                        i - 1, newdata.columns.get_loc("trip end battery charge")
-                    ]
-                    + newdata.iloc[
-                        i - 1, newdata.columns.get_loc("charging consumption")
-                    ]
-                )
-                trip_num += 1
-                newdata.iloc[i, newdata.columns.get_loc("trip number")] = trip_num
-
-            else:
-                newdata.iloc[
-                    i, newdata.columns.get_loc("trip start battery charge")
-                ] = kwh
-                trip_num = 1
-                newdata.iloc[i, newdata.columns.get_loc("trip number")] = trip_num
-
-            if (
-                newdata.iloc[i, newdata.columns.get_loc("total vehicle miles traveled")]
-                < veh_range * const.ER
-            ):
-                newdata.iloc[i, newdata.columns.get_loc("BEV could be used")] = 1
-                newdata.iloc[i, newdata.columns.get_loc("trip end battery charge")] = (
-                    newdata.iloc[
-                        i, newdata.columns.get_loc("trip start battery charge")
-                    ]
-                    - newdata.iloc[i, newdata.columns.get_loc("Miles traveled")]
-                    * kwhmi
-                    * const.ER
-                )
-            else:
-                newdata.iloc[i, newdata.columns.get_loc("BEV could be used")] = 0
-
-            trip_data = newdata.iloc[i]
-
-            newdata.iloc[
-                i, newdata.columns.get_loc("charging power")
-            ] = charging.get_charging_power(
-                power,
-                trip_strategy,
-                location_strategy,
-                kwh,
-                trip_data,
-            )
-
-            newdata.iloc[
-                i, newdata.columns.get_loc("charging time")
-            ] = charging.get_charging_time(
-                newdata.iloc[i, newdata.columns.get_loc("charging power")],
-                kwh,
-                newdata.iloc[i, newdata.columns.get_loc("trip end battery charge")],
-                const.charging_efficiency,
-                trip_data,
-            )
-
-            newdata.iloc[i, newdata.columns.get_loc("charging consumption")] = (
-                newdata.iloc[i, newdata.columns.get_loc("charging power")]
-                * newdata.iloc[i, newdata.columns.get_loc("charging time")]
-                * const.charging_efficiency
-            )
-
-            # charging start point
-            start_point = round(
-                newdata.iloc[i, newdata.columns.get_loc("End time (hour decimal)")]
-                * 100
-            )
-
-            # charging end point
-            end_point = start_point + round(
-                newdata.iloc[i, newdata.columns.get_loc("charging time")] * 100
-            )
-            weekday_electricload[start_point:end_point] += newdata.iloc[
-                i, newdata.columns.get_loc("charging power")
-            ]
-
-    weekday_output_electricload = np.zeros(48)
-    for k in range(48):
-        if k == 0:
-            weekday_output_electricload[k] = sum(weekday_electricload[:100]) / 100
-        elif k == 47:
-            weekday_output_electricload[k] = sum(weekday_electricload[4700:]) / 100
+    model_year_profile = np.zeros(24 * len(input_day))
+    flag_translation = {1: "weekend", 2: "weekday"}
+    for i, weekday_flag in enumerate(input_day):
+        daily_profile = daily_resampled_profiles[flag_translation[weekday_flag]]
+        if i == len(input_day) - 1:
+            # for the last day of the year, we need to wrap our 48 hours back around
+            # first half: last day of the year
+            model_year_profile[-24:] += daily_profile[:24]
+            # last half: first day of the year
+            model_year_profile[:24] += daily_profile[24:]
         else:
-            weekday_output_electricload[k] = (
-                sum(weekday_electricload[(k + 1) * 100 - 150 : (k + 1) * 100 - 50])
-                / 100
-            )
+            model_year_profile[i * 24 : i * 24 + 48] += daily_profile
+    # Normalize the output so that it sums to 1
+    summed_profile = model_year_profile / model_year_profile.sum()
 
-    # weekend, reset vars
-    trip_num = 1
-    start_point = 0
-    end_point = 0
-
-    for i in range(l):
-        if data_day[i] == 1:  # 1 is weekend
-            if (
-                i > 0
-                and newdata.iloc[i, newdata.columns.get_loc("sample vehicle number")]
-                == newdata.iloc[i - 1, newdata.columns.get_loc("sample vehicle number")]
-            ):
-                newdata.iloc[
-                    i, newdata.columns.get_loc("trip start battery charge")
-                ] = (
-                    newdata.iloc[
-                        i - 1, newdata.columns.get_loc("trip end battery charge")
-                    ]
-                    + newdata.iloc[
-                        i - 1, newdata.columns.get_loc("charging consumption")
-                    ]
-                )
-                trip_num += 1
-                newdata.iloc[i, newdata.columns.get_loc("trip number")] = trip_num
-
-            else:
-                newdata.iloc[
-                    i, newdata.columns.get_loc("trip start battery charge")
-                ] = kwh
-                trip_num = 1
-                newdata.iloc[i, newdata.columns.get_loc("trip number")] = trip_num
-
-            if (
-                newdata.iloc[i, newdata.columns.get_loc("total vehicle miles traveled")]
-                < veh_range * const.ER
-            ):
-                newdata.iloc[i, newdata.columns.get_loc("BEV could be used")] = 1
-                newdata.iloc[i, newdata.columns.get_loc("trip end battery charge")] = (
-                    newdata.iloc[
-                        i, newdata.columns.get_loc("trip start battery charge")
-                    ]
-                    - newdata.iloc[i, newdata.columns.get_loc("Miles traveled")]
-                    * kwhmi
-                    * const.ER
-                )
-
-            else:
-                newdata.iloc[i, newdata.columns.get_loc("BEV could be used")] = 0
-
-            trip_data = newdata.iloc[i]
-
-            newdata.iloc[
-                i, newdata.columns.get_loc("charging power")
-            ] = charging.get_charging_power(
-                power,
-                trip_strategy,
-                location_strategy,
-                kwh,
-                trip_data,
-            )
-
-            newdata.iloc[
-                i, newdata.columns.get_loc("charging time")
-            ] = charging.get_charging_time(
-                newdata.iloc[i, newdata.columns.get_loc("charging power")],
-                kwh,
-                newdata.iloc[i, newdata.columns.get_loc("trip end battery charge")],
-                const.charging_efficiency,
-                trip_data,
-            )
-
-            newdata.iloc[i, newdata.columns.get_loc("charging consumption")] = (
-                newdata.iloc[i, newdata.columns.get_loc("charging power")]
-                * newdata.iloc[i, newdata.columns.get_loc("charging time")]
-                * const.charging_efficiency
-            )
-
-            start_point = round(
-                newdata.iloc[i, newdata.columns.get_loc("End time (hour decimal)")]
-                * 100
-            )
-            end_point = start_point + round(
-                newdata.iloc[i, newdata.columns.get_loc("charging time")] * 100
-            )
-            weekend_electricload[start_point:end_point] += newdata.iloc[
-                i, newdata.columns.get_loc("charging power")
-            ]
-
-    weekend_output_electricload = np.zeros(48)
-    for k in range(48):
-        if k == 0:
-            weekend_output_electricload[k] = sum(weekend_electricload[:100]) / 100
-        elif k == 47:
-            weekend_output_electricload[k] = sum(weekend_electricload[4700:]) / 100
-        else:
-            weekend_output_electricload[k] = (
-                sum(weekend_electricload[(k + 1) * 100 - 150 : (k + 1) * 100 - 50])
-                / 100
-            )
-
-    outputs = [weekend_output_electricload, weekday_output_electricload]
-
-    for day_iter in range(len(input_day)):
-        # weekday = 1 if weekday, 0 if not weekday
-        weekday = input_day[day_iter] - 1
-
-        if day_iter == len(input_day) - 1:
-            TRANS_charge[day_iter * 24 :] += outputs[weekday][:24].conj().T
-            TRANS_charge[:24] += outputs[weekday][24:48].conj().T
-
-        else:
-            TRANS_charge[day_iter * 24 : day_iter * 24 + 48] += (
-                outputs[weekday].conj().T
-            )
-
-    TRANS_charge = TRANS_charge / sum(TRANS_charge)
-
-    return TRANS_charge
+    return summed_profile
 
 
 def adjust_BEV(TRANS_charge, adjustment_values):
