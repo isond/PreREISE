b18200a0b7d90576c96dcfded8ce07eb1f33f736
Dianne Ison <isond@uci.edu>
2022-09-01 18:00:36 -0700

    test: update smart charging HDV tests

--- prereise/gather/demanddata/transportation_electrification/tests/test_smart_charging.py
+++ prereise/gather/demanddata/transportation_electrification/tests/test_smart_charging.py
@@ -5,6 +5,7 @@ import numpy as np
 from scipy.io import loadmat
 
 import prereise
+from prereise.gather.demanddata.transportation_electrification import data_helper
 from prereise.gather.demanddata.transportation_electrification.data_helper import (
     generate_daily_weighting,
 )
@@ -102,12 +103,28 @@ def run_smart_charging_HDV():
     )
     load_demand = loadmat(data_dir)["load_demand"].flatten()
 
+    bev_vmt = data_helper.load_urbanized_scaling_factor(
+        model_year=2050,
+        veh_type="HDV",
+        veh_range=200,
+        urbanized_area="Antioch",
+        state="CA",
+        filepath=os.path.join(
+            os.path.dirname(inspect.getsourcefile(prereise)),
+            "gather",
+            "demanddata",
+            "transportation_electrification",
+            "data",
+            "regional_scaling_factors",
+            "Regional_scaling_factors_UA_",
+        ),
+    )
     result = smart_charging_HDV.smart_charging(
+        model_year=2050,
         veh_range=200, 
-        kwhmi_f=[0.32, 0.85, 1.64], 
         power=80, 
         location_strategy=1, 
-        veh_type="HHDV", 
+        veh_type="HDV", 
         filepath=os.path.join(
             os.path.dirname(inspect.getsourcefile(prereise)),
             "gather",
@@ -116,21 +133,83 @@ def run_smart_charging_HDV():
             "fdata_v10st.data",
         ),
         initial_load=load_demand, 
-        hdv_year=6, 
-        hdv_penetration=[0.765, 0.696, 0.219], 
+        bev_vmt=bev_vmt,
         trip_strategy=1
     )
 
     correct_cumsum = np.array(
         [
-            1380.30981545918,
-            1391857.506,
-            2784877.62,
-            4154503.883,
-            5549228.041,
-            6930893.731,
-            8311924.876,
-            9704099.913,
+            1.22854177233283, 
+            4729.417063,
+            9456.028814,
+            14087.49171,
+            18817.56654,
+            23521.75604,
+            28175.75066,
+            32904.52077, 
+        ]
+    )
+
+    np.testing.assert_allclose(result.cumsum()[::1095], correct_cumsum)
+
+
+def test_smart_charging_MDV():
+    run_smart_charging_MDV()
+
+def run_smart_charging_MDV():
+    data_dir = os.path.join(
+        os.path.dirname(inspect.getsourcefile(prereise)),
+        "gather",
+        "demanddata",
+        "transportation_electrification",
+        "CAISO_sample_load_2019.mat",
+    )
+    load_demand = loadmat(data_dir)["load_demand"].flatten()
+
+    bev_vmt = data_helper.load_urbanized_scaling_factor(
+        model_year=2050,
+        veh_type="MDV",
+        veh_range=200,
+        urbanized_area="Antioch",
+        state="CA",
+        filepath=os.path.join(
+            os.path.dirname(inspect.getsourcefile(prereise)),
+            "gather",
+            "demanddata",
+            "transportation_electrification",
+            "data",
+            "regional_scaling_factors",
+            "Regional_scaling_factors_UA_",
+        ),
+    )
+    result = smart_charging_HDV.smart_charging(
+        model_year=2050,
+        veh_range=200, 
+        power=80, 
+        location_strategy=1, 
+        veh_type="MDV", 
+        filepath=os.path.join(
+            os.path.dirname(inspect.getsourcefile(prereise)),
+            "gather",
+            "demanddata",
+            "transportation_electrification",
+            "fdata_v10st.data",
+        ),
+        initial_load=load_demand, 
+        bev_vmt=bev_vmt,
+        trip_strategy=1
+    )
+
+    correct_cumsum = np.array(
+        [
+            0.291225084060603, 
+            4160.770734,
+            8320.031013,
+            12391.66618,
+            16553.00332,
+            20705.85663,
+            24784.48369,
+            28946.34624,  
         ]
     )
 
